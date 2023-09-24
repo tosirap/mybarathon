d87134764386e9272e30916fd30e71f8eeb727d1
tosirap <parisotpaul@live.fr>
2023-09-24 14:05:59 +0200

    billeterie

--- components/barathon/Billeterie..vue
+++ components/barathon/Billeterie..vue
@@ -2,20 +2,19 @@
     <div class="content" >
       <h1 class="centered"  style="padding-bottom: 24px;"><b>Billeterie</b></h1>
         <div class="billeterie">
-          <a
-            title="Vente de billets en ligne"
-            href="https://www.billetweb.fr/shop.php?event=grand-my-barathon-de-saint-patrick-2023-a-strasbourg-par-mybarathon"
-            class="shop_frame"
-            target="_blank"
-            data-src="https://www.billetweb.fr/shop.php?event=grand-my-barathon-de-saint-patrick-2023-a-strasbourg-par-mybarathon"
-            data-max-width="100%"
-            data-initial-height="600"
-            data-scrolling="no"
-            data-id="grand-my-barathon-de-saint-patrick-2023-a-strasbourg-par-mybarathon"
+          <a 
+            title="Vente de billets en ligne" 
+            href="https://www.billetweb.fr/shop.php?event=barathon2" 
+            class="shop_frame"  
+            target="_blank"  
+            data-src="https://www.billetweb.fr/shop.php?event=barathon2"  
+            data-max-width="100%"  
+            data-initial-height="600" 
+            data-scrolling="no"  
+            data-id="barathon2"  
             data-resize="1"
           >
-            Vente de billets en ligne
-          </a>
+          Vente de billets en ligne</a>
         </div>
     </div>
 </template>
