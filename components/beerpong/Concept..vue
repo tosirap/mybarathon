d26b97d1d6c39c8788892c31f3efc77a20b28fff
tosirap <parisotpaul@live.fr>
2023-11-09 04:25:52 +0100

    fix orthographe

--- components/beerpong/Concept..vue
+++ components/beerpong/Concept..vue
@@ -3,15 +3,15 @@
       <h1 class="centered w3-padding-16"><b>Concept</b></h1>
       <h4 class="centered" >SAMEDI 18 NOVEMBRE 14h au <a href="https://www.google.com/maps/place/Delirium+Caf%C3%A9/@48.5846828,7.7424422,15z/data=!4m6!3m5!1s0x4796c84c1d430bf1:0x6d86f9bc1635704d!8m2!3d48.5846828!4d7.7424422!16s%2Fg%2F11g9sx2l3b?entry=ttu" style="text-decoration: none;">Délirium</a>. </h4>
         <p> 
-          Principe du BeerPong made in MyBarathon :
+          Principe du Beer Pong made in MyBarathon :
         </p>
         <ul class="concept-text">
           <li>Le tournoi se déroule par équipe de 3 participants.</li>
           <li>Le tournoi commence par une phase de poule de 3 équipes puis passe en élimination directe.
-            <br> Cela permet au équipes les moins fortes de jouer au minimum 2 matchs.</li>
+            <br>Cela permet aux équipes les moins fortes de jouer au minimum 2 matchs.</li>
           <li>Les gobelets seront remplis par de la bière ou un soft (pour les softs nous prévenir à l'avance).</li>
           <li>
-            10€ par joueur (comprend la bière/soft de l'intégralité des matchs du tournois).
+            10€ par joueur (comprends la bière/soft de l'intégralité des matchs du tournoi).
           </li>
         </ul>
         <p class="concept-text centered">
--- components/beerpong/Regles.vue
+++ components/beerpong/Regles.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="content">
       <h1 class="centered w3-padding-16"><b>Règles</b></h1>
-      <h4 class="centered"> Les 10 commandemants du Beer Pong</h4>
+      <h4 class="centered"> Les 10 commandements du Beer Pong</h4>
       <ol class="rules">
-        <li> Chaque joueur d'une équipe lance 1 balle. Quand tous les joueurs d'une équipe ont joué on passe a l'équipe suivante. </li>
-        <li> Si la balle rentre directement dans 1 gobelet on retire 1 verre.  </li>
-        <li> Si la balle rentre dans un gobelet avec un rebond on retire 2 verres. </li>
-        <li> Si 2 balles rentre dans le même gobelet on retire 3 verres (on ajoute +1 pour chaque balle rentré avec rebond). </li>
-        <li> Si 3 balles rentre dans le même gobelet : game over la partie s'arrête vous avez gagné. </li>
-        <li> On peut défendre ses verres dès que la balle fait un rebond (en cas de défense illégal on retire un verre). </li>
+        <li> Chaque joueur d'une équipe lance 1 balle. Quand tous les joueurs d'une équipe ont joué, on passe à l'équipe suivante. </li>
+        <li> Si la balle rentre directement dans 1 gobelet, on retire 1 verre.  </li>
+        <li> Si la balle rentre dans un gobelet avec un rebond, on retire 2 verres. </li>
+        <li> Si 2 balles rentrent dans le même gobelet, on retire 3 verres (on ajoute +1 pour chaque balle rentrée avec rebond). </li>
+        <li> Si 3 balles rentrent dans le même gobelet : game over la partie s'arrête, vous avez gagné. </li>
+        <li> On peut défendre ses verres dès que la balle fait un rebond (en cas de défense illégale on retire un verre). </li>
         <li> On peut souffler dans le verre tant que la balle n'a pas touché l'eau. </li>
         <li> Si la balle revient vers nous sur la table, on peut s'en saisir dans notre moitié et la relancé en réalisant une figure. </li>
-        <li> Si tous les membres d'une équipe marque sur leur tour alors l'équipe rejoue. </li>
-        <li> Si lors d'un lancé la balle ne touche rien (ni table, ni gobelet) c'est un airball, le tireur retire un verre. </li>
+        <li> Si tous les membres d'une équipe marquent sur leur tour alors l'équipe rejoue. </li>
+        <li> Si lors d'un lancer la balle ne touche rien (ni table, ni gobelet) c'est un airball, le tireur retire un verre. </li>
       </ol>
     </div>
 </template>
@@ -22,6 +22,9 @@ ol.rules{
         font: italic 20px Georgia, Times, serif;
         padding-bottom: 10px;
         padding-top: 5px;
-        line-height: 35px;
+        line-height: 30px;
+}
+ol.rules li{
+  padding-bottom: 12px;
 }
 </style>
